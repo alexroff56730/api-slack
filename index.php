dfb06f3ad0975834cec31befb0bb74949c1cff75
timote56 <alexandreroffiaen@gmail.com>
2021-01-28 11:39:12 +0100

    ilovepoussin

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 
-$a = "fuck";
-$b = "you";
+$a = "<html>
+        <head>
+            <tite>POUSSIIIN</titre>
+        </head>
+        <body style='background-color:black;'>
+            <div>
+                <p style='color=white; font-size:2.5rem;'>
+                    je t'aime poussin ! 
+                </p>
+            </div>
+        </body>
+    </html>"
 
-echo($a . " " . $b);
+echo($a);
